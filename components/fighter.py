cd7bc81d07aa5acd7ff92c184ad4db43a674327f
Arsh Gupta <arsh121523@gmail.com>
2025-01-01 20:38:20 +0530

    added messages in game

--- components/fighter.py
+++ components/fighter.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import color
 from typing import TYPE_CHECKING
 from input_handlers import GameOverEventHandler
 from components.base_component import BaseComponent
@@ -28,9 +28,11 @@ class Fighter(BaseComponent):
     def die(self) -> None:
         if self.engine.player is self.entity:
             death_message = "You died!"
+            death_message_color = color.player_die
             self.engine.event_handler = GameOverEventHandler(self.engine)
         else:
             death_message = f"{self.entity.name} is dead!"
+            death_message_color = color.enemy_die
 
         self.entity.char = "%"
         self.entity.color = (191, 0, 0)
@@ -39,4 +41,4 @@ class Fighter(BaseComponent):
         self.entity.name = f"remains of {self.entity.name}"
         self.entity.render_order = RenderOrder.CORPSE
 
-        print(death_message)
+        self.engine.message_log.add_message(death_message, death_message_color)

34db4cdaee4056564318e8593b2defa85b0ad433
Arsh Gupta <arsh121523@gmail.com>
2025-01-02 13:46:45 +0530

    adding mouse location to engine

--- engine.py
+++ engine.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 from render_functions import render_bar
-from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 from message_log import MessageLog
@@ -20,6 +19,7 @@ class Engine:
     def __init__(self, player: Actor):
         self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
+        self.mouse_location = (0, 0)
         self.message_log = MessageLog()
 
     def handle_enemy_turns(self) -> None:
@@ -37,7 +37,7 @@ class Engine:
         # If a tile is "visible" it should be added to "explored".
         self.game_map.explored |= self.game_map.visible
 
-    def render(self, console: Console, context: Context) -> None:
+    def render(self, console: Console) -> None:
         self.game_map.render(console)
         self.message_log.render(console=console, x=21, y=45, width=40, height=5)
 
@@ -47,7 +47,3 @@ class Engine:
             maximum_value=self.player.fighter.max_hp,
             total_width=20,
         )
-
-        context.present(console)
-
-        console.clear()

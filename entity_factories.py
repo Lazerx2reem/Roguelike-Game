641bb600d349d97838c562bfb02e283b8884489a
Arsh Gupta <arsh121523@gmail.com>
2025-01-05 11:34:05 +0530

    Added confusion scroll in entity factories

--- entity_factories.py
+++ entity_factories.py
@@ -29,15 +29,20 @@ troll = Actor(
     fighter=Fighter(hp=16, defense=1, power=4),
     inventory=Inventory(capacity=0),)
 
+confusion_scroll = Item(
+    char="~",
+    color=(207, 63, 255),
+    name="Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),)
+
 health_potion = Item(
     char="!",
     color=(127, 0, 255),
     name="Health Potion",
-    consumable=consumable.HealingConsumable(amount=4),
- )
+    consumable=consumable.HealingConsumable(amount=4),)
+
 lightning_scroll = Item(
     char="~",
     color=(255, 255, 0),
     name="Lightning Scroll",
-    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
- )
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),)

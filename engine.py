2e90266bf98bd56208e92c1a372aa37be1989c62
Arsh Gupta <arsh121523@gmail.com>
2025-01-06 13:33:44 +0530

    updated engine

--- engine.py
+++ engine.py
@@ -5,20 +5,17 @@ from render_functions import render_bar, render_names_at_mouse_location
 from tcod.console import Console
 from tcod.map import compute_fov
 from message_log import MessageLog
-from input_handlers import MainGameEventHandler
 import exceptions
 
 if TYPE_CHECKING:
     from entity import Actor
     from game_map import GameMap
-    from input_handlers import EventHandler
 
 
 class Engine:
     game_map: GameMap
 
     def __init__(self, player: Actor):
-        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
         self.mouse_location = (0, 0)
         self.message_log = MessageLog()

fd90ace71178df65e922b9493d54f5c9a4643e9e
Arsh Gupta <arsh121523@gmail.com>
2025-01-08 12:26:05 +0530

    Updated engine to render functions

--- render_functions.py
+++ render_functions.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Tuple, TYPE_CHECKING
 
 import color
 
@@ -19,6 +19,16 @@ def get_names_at_location(x: int, y: int, game_map: GameMap) -> str:
     )
 
     return names.capitalize()
+
+def render_dungeon_level(
+    console: Console, dungeon_level: int, location: Tuple[int, int]
+ ) -> None:
+    """
+    Render the level the player is currently on, at the given location.
+    """
+    x, y = location
+
+    console.print(x=x, y=y, string=f"Dungeon level: {dungeon_level}")
 
 
 def render_bar(
